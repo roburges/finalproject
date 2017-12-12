fd9c7729f1b3209e85296503456a77ff0f6b3ef8
roburges <roburges@umich.edu>
2017-12-11 23:58:07 -0500

    datawithjson

--- wsjdata2.py
+++ wsjdata2.py
@@ -15,34 +15,39 @@ try:
 except:
     CACHE_DICTION = {}
 # Define your function get_user_tweets here:
-params = {}
-params['sources'] = "the-wall-street-journal"
-params['q']="Apple"
-params['from']='2016-1-1'
-params['limit']=100
-params['apiKey'] = "20c32860b43c40dc89c7654bb9140192"
+urlparams = {}
+urlparams['sources'] = "the-wall-street-journal"
+urlparams['q']="Apple"
+urlparams['from']='2016-1-1'
+urlparams['limit']=100
+urlparams['apiKey'] = "20c32860b43c40dc89c7654bb9140192"
 url = ('https://newsapi.org/v2/everything?')
-def get_user_tweets(price):
+def gettingapplearticle(article):
 	# CACHE_DICTION={}
 	#tweets='@umich' ##gives variable to key word for easy editing later
-    if price in CACHE_DICTION: ##LOOP, READ, AND ASSIGN RESULTS VARIABLE FOR CACHE DICTION
-        results=CACHE_DICTION[price]
+    if article in CACHE_DICTION: ##LOOP, READ, AND ASSIGN RESULTS VARIABLE FOR CACHE DICTION
+        results=CACHE_DICTION[article]
         print('using cached data')##let user know data was in cache
     else:
         print('getting data from the internet')##let user know not in cache/scraping
-        results= requests.get(url, params)
-        CACHE_DICTION[price]=results
-        f=open(CACHE_FNAME,'w')##open file internet option to cache
-        f.write(json.dumps(CACHE_DICTION))##write scrape to cache for json
-        f.close()##close file
-    return(results)
-    print(results)
-# description = results['articles'][0]['description']
-# title = results['articles'][0]['title']
-#
-# params = {}
-# params['sources'] = "newyorktimes"
-# params['apiKey'] = "87c4d119d268416dabca815fb87cdbbb"
-# base_url = "https://api.nytimes.com/svc/topstories/v2/home.json" #search top_stories
+        results= requests.get(url, params=urlparams)
+        CACHE_DICTION[article]=json.loads(results.text)
+        intdict=json.dumps(CACHE_DICTION)##write scrape to cache for jso
+        f=open(CACHE_FNAME,'w')##open file internet option to cach
+        f.write(intdict)
+        f.close()
 
-#pprint(description)
+    return(results)
+articles=gettingapplearticle('apple')
+conn = sqlite3.connect('finalproject.sqlite')
+cur = conn.cursor()
+#create table within database for Facebook API
+cur.execute("DROP TABLE IF EXISTS WSJ_Articles")
+cur.execute('''CREATE TABLE WSJ_Articles (title TEXT,  Date_Created DATETIME)'''
+#looping through every post and retrieving the post id, created time, message, story
+#from created time, finding the weekday and time period
+for apple in gettingapplearticle:
+
+
+    tup=correct_title=apple['title'],apple['publishedAt']
+    cur.execute('''INSERT or IGNORE INTO WSJ_Articles(title, Date_Created)''' VALUES (?,?,?,?),tup)
